9a58d249b2bc4a16f4dc172fb5469f7363229da2
Aaron Hsu <aaron@nextbit.com>
2020-02-05 21:39:33 -0500

    Simple

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/ahsu1230/GolangCITest/morenum"
-	"github.com/ahsu1230/GolangCITest/morestrings"
-	"github.com/google/go-cmp/cmp"
+	// "github.com/ahsu1230/GolangCITest/morenum"
+	// "github.com/ahsu1230/GolangCITest/morestrings"
+	// "github.com/google/go-cmp/cmp"
 )
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
-	fmt.Println(morenum.Add(3,4))
+	fmt.Println("Hello")
+	// fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	// fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	// fmt.Println(morenum.Add(3,4))
 }
